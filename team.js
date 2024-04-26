03cc11f6ce581fd32e9806c19f44054657935c20
Maurilio Valenti <maurilio.valenti@gmail.com>
2024-04-26 19:49:26 +0200

    Create new array

---
+++ team.js
@@ -0,0 +1,14 @@
+const person = [
+  {
+    name: "Maurilio",
+    surname: "Valenti",
+    age: 31,
+    city: "Palermo",
+    hobby: "Padel",
+    favoriteFood: "Pasta",
+    favoriteVideoGame: "Fc 24",
+    favoriteFilm: "Django",
+    favoriteBook: "Dieci piccoli indiani",
+    petName: "Kira",
+  },
+];
