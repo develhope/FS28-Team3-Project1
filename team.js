a7d940ec2696c7a804c97bae7d9f7ede86f4eafb
Samuele Berti <samuelebrt4@gmail.com>
2024-05-02 12:59:41 +0200

    Samuele Berti info

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-const person = []
+const person = [ {
+    name: "Samuele",
+    surname: "Berti",
+    age: 21,
+    city: "Manciano",
+    hobby: "Leggere",
+    favoriteFood: "Lasagna",
+    favoriteVideoGame: "Wrc8",
+    favoriteFilm: "L'ultimo dei moicani",
+    favoriteBook: "Il signore delle Mosche",
+    petName: "Luna",
+  }]
