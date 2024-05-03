e2c0d9469f44893db28ae9a1c45ff9765d42bb35
Stanislao95 <166648506+Stanislao95@users.noreply.github.com>
2024-05-03 20:32:44 +0200

    coomit stanislao

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-console.log('Ciao!');
+const person = {
+  name: "Stanislao",
+  surname: "Tufo",
+  age: 28,
+  city: "Apice",
+  hobby: "Djing",
+  favoriteFood: "Pizza",
+  favoriteVideoGame: "F12023",
+  favoriteFilm: "MissioImpossibleProtocolloFantasma",
+  favoriteBook: "LoScudoDiThalos",
+  petName: "Fiocco",
+};
