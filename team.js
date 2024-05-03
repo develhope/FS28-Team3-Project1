98e9fe9048b3a232a59398deecdceda6ebf01402
Paolo Scapinello <pscapinello@gmail.com>
2024-05-03 17:11:42 +0200

    "creato array"

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-console.log('Ciao!');
+ const person = [ {
+    name: "Paolo",
+    surname: "Scapinello",
+    age: 24,
+    city: "Padova",
+    hobby: "Giocare/Leggere/Gurdare tv o film",
+    favoriteFood: "Pasta al forno",
+    favoriteVideoGame: "Elden Ring",
+    favoriteFilm: "Blade Runner 2049",
+    favoriteBook: "Dracula",
+    petName: "Luca",
+  }]
