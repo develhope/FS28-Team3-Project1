169de93574fa838d9e5beeeb59a36cf6086a4fff
Nunzia Biele <nunzia.biele@virgilio.it>
2024-05-03 11:54:15 +0200

    Nunzia Biele info

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-const person = []
+const person = [ {
+    name: "Nunzia",
+    surname: "Biele",
+    age: 28,
+    city: "Benevento",
+    hobby: "Drawing",
+    favoriteFood: "Pizza",
+    favoriteVideoGame: "Assassin's creed",
+    favoriteFilm: "Le pagine della nostra vita",
+    favoriteBook: "Your Name",
+    petName: "Lucky",
+  },]
