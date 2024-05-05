1f654693dc3fdc5a0bb4787dd4b710728b8fbc41
SBerti <samuelebrt4@gmail.com>
2024-05-05 21:44:24 +0200

    Revert "coomit stanislao"

--- team.js
+++ team.js
@@ -1,12 +1,1 @@
-const person = {
-  name: "Stanislao",
-  surname: "Tufo",
-  age: 28,
-  city: "Apice",
-  hobby: "Djing",
-  favoriteFood: "Pizza",
-  favoriteVideoGame: "F12023",
-  favoriteFilm: "MissioImpossibleProtocolloFantasma",
-  favoriteBook: "LoScudoDiThalos",
-  petName: "Fiocco",
-};
+console.log('Ciao!');
